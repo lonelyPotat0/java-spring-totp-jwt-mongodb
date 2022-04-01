823725367316c9a271f67853e0ce5fff79f4b851
dom <52703619+lonelyPotat0@users.noreply.github.com>
2022-04-01 14:42:54 +0700

    fix duplicate username

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -1,6 +1,7 @@
 package com.example.demo.models;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -9,7 +10,9 @@ public class User {
     @Id
     private String id;
     
+    @Indexed(unique = true)
     private String username;
+    
     private String password;
     private String authKey;
     private Boolean tfa;
